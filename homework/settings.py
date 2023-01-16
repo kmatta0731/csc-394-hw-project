f6ee9dff7db2a85795d0d29766ac5339350583aa
Kathie Matta <kathiematta1@gmail.com>
2023-01-16 17:19:18 -0600

    second Commit

--- homework/settings.py
+++ homework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ld%_3cc0+q2map)0xu2eq#aloocs*1mmij-xu#j(f6#lhzzvr&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'homework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'localhost',
+        'USER': 'kathiematta',
+        'PASSWORD': 'pulgoso1',
+        'HOST': '0.0.0.0',
+        'PORT': '5432',
     }
 }
 
